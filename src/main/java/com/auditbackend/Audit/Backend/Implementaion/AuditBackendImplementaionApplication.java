4025d3f630c70108705dbdb5ac9496c64ffbecde
Anup <anooptiwari4766@gmail.com>
2025-08-13 00:50:56 +0530

    Initial Setup

---
+++ src/main/java/com/auditbackend/Audit/Backend/Implementaion/AuditBackendImplementaionApplication.java
@@ -0,0 +1,13 @@
+package com.auditbackend.Audit.Backend.Implementaion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuditBackendImplementaionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuditBackendImplementaionApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/auditbackend/Audit/Backend/Implementaion/AuditBackendImplementaionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.auditbackend.Audit.Backend.Implementaion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuditBackendImplementaionApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
